1479a20bb035e110f2fb65aa9a17206e67df296a
Sama Alharbi <176575888+samaalharbi2@users.noreply.github.com>
2025-11-06 09:53:07 +0300

    Delete target directory

--- target/compiled/snowflake_data_project/models/marts/fct_daily_order_revenue.sql
+++
@@ -1,9 +0,0 @@
-select
-o.order_date,
-O.order_id,
-sum(total_price) AS total_price
-from
-finance_db.raw.stg_orders O
-LEFT JOIN finance_db.raw.stg_orders_items OI
-ON O.ORDER_ID=OI.ORDER_ID
-GROUP BY 1,2
--- target/compiled/snowflake_data_project/models/staging/stg_customers.sql
+++
@@ -1,6 +0,0 @@
-SELECT
-id AS customer_id,
-name AS customer_name ,
-email,
-country
-FROM finance_db.raw.customers
